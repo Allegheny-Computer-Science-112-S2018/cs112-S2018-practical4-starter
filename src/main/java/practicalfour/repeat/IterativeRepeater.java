705cb513ee6b09ddf7a91e787ea2e4aedbc16c4e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-02-23 13:14:42 -0500

    Adding the first version of the IterativeRepeater.

---
+++ src/main/java/practicalfour/repeat/IterativeRepeater.java
@@ -0,0 +1,22 @@
+package practicalfour.repeat;
+
+/**
+ * A class that iteratively constructs a string of a repeated character.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class IterativeRepeater extends Repeater {
+
+  /** Set the name of the IterativeRepeater. */
+  public IterativeRepeater() {
+    // TODO: Add the name
+  }
+
+  /** Use repeated concatenation to compose a String with "repeats" copies of a
+   *  specified "character". */
+  public String repeat(char character, int repeats) {
+    // TODO: Add the method implementation from the textbook
+  }
+
+}
