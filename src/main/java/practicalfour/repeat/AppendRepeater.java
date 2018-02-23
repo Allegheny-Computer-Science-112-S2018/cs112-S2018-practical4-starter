2da269221f8f6df9bfe77208bc5bf01b10493d4e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-02-23 13:13:54 -0500

    Adding in the AppendRepeater.

---
+++ src/main/java/practicalfour/repeat/AppendRepeater.java
@@ -0,0 +1,22 @@
+package practicalfour.repeat;
+
+/**
+ * A class that constructs a string of a repeated character with appends.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class AppendRepeater extends Repeater {
+
+  /** Set the name of the AppendRepeater. */
+  public AppendRepeater() {
+      // TODO: Add the name
+  }
+
+  /** Use repeated concatenation to compose a String with "repeats" copies of a
+   *  specified "character". */
+  public String repeat(char character, int repeats) {
+      // TODO: Add the correct repeat method from the textbook
+  }
+
+}
